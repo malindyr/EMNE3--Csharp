4ea64ef77f39fd0707b44c420cfb5cd574ceca2b
malindyr <malin.dyrnes2@gmail.com>
2024-05-15 13:30:30 +0200

    .

---
+++ 10.05.2024/10.05.2024 solving moodle assignments/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+    int tall1 = 1;
+    int tall2 = 2;
+    public int ReturnANumber( int number, int number2)
+    {
+        return number + number2;
+
+    }
+
+    public void op()
+    {
+        var number = Console.ReadLine();
+        var number2 = Console.ReadLine();
+
+
+        Console.WriteLine(ReturnANumber(tall1, tall2));
+        
+
+        
+    }
+}
---
+++ 11.05.24/11.05.24 moodle igjen/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    int points = 0;
+    public void generateNumbers()
+    {
+        Random rand = new Random();
+        int randomNr1 = rand.Next(0, 6);
+        int randomNr2 = rand.Next(0, 6);
+
+        displayNumbers(randomNr1, randomNr2);
+    }
+    public void displayNumbers(int nr1, int nr2)
+    {
+        Console.WriteLine($"{nr1}_{nr2}");
+        var input = Console.ReadLine();
+
+        if (nr1 < nr2 && input == "<")
+        {   points++;
+            Console.WriteLine("correct!");
+            Console.WriteLine($"points: {points}");
+            generateNumbers();
+        }
+        else if (nr2 < nr1 && input == ">")
+        {   points++;
+            Console.WriteLine("correct!");
+            Console.WriteLine($"points: {points}");
+            generateNumbers();
+        }
+        else if (nr1 == nr2 && input == "=")
+        {   points++;
+            Console.WriteLine("corrct!");
+            Console.WriteLine($"points: {points}");
+            generateNumbers();
+        }
+        else if (input !="<" && input !=">" && input !="=")
+        {   Console.WriteLine("game over:(");
+        }
+        else
+        {   points = points - 1;
+            Console.WriteLine("wrong");
+            Console.WriteLine($"points: {points}");
+            generateNumbers();
+        }}
+    static void Main()
+    {
+        Program run = new Program();
+        run.generateNumbers();
+    }
+}
