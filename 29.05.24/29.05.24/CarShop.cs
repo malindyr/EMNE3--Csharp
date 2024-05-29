5199ddca0c75f6b8614341e5596afd6cd9bdae98
malindyr <malin.dyrnes2@gmail.com>
2024-05-29 14:03:33 +0200

    .

---
+++ 29.05.24/29.05.24/CarShop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29._05._24
+{
+    internal class CarShop
+    {
+    }
+}
---
+++ 29.05.24/29.05.24/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Bilforhandleren
+En bilforhandler har ulike biler i shappa si,
+det kan være biler med ulike merker, årsmodell, 
+registreringsnummer og kilometerstand. 
+Når kunden kommer inn i shappa skal han kunne se hvilke biler som finnes i shappa, 
+eller han kan velge å finne en bil ut fra årsrange, 
+eller en bil som har kjørt mer eller mindre enn en gitt kilometerstand. Kunden skal også ha mulighet til å kjøpe en av bilene. 
+Bilen må da tilhøre kunden og ikke forhandleren.*/
+
+  static void Main()
+{
+
+}
